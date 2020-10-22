fa21246ccdf4af8cb5f9b6f4f4ed0d2b206af77b
Roshan Acharya <acharyaroshan2357@gmail.com>
2020-10-22 14:43:33 +0545

    feat: add support for ulka 0.6.6

--- ulka-source-ejs/plugin/index.js
+++ ulka-source-ejs/plugin/index.js
@@ -1,77 +1,22 @@
-const fs = require('fs')
 const ejs = require('ejs')
-const url = require('url')
-const path = require('path')
 
-exports.beforeBuild = ({ info, pagesArray }, { ejsOptions = {} } = {}) => {
-  const allEjsFiles = allFiles(info.configs.pagesPath)
+exports.beforeSetup = (
+  { info },
+  { pages = true, contents = true, ejsOptions = {} } = {}
+) => {
+  // Dont copy .ejs files to __assets__ while building
+  info.ignoreExtensions.push('.ejs')
 
-  for (const file of allEjsFiles) {
-    const raw = fs.readFileSync(file, 'utf-8')
-    const stats = fs.statSync(file)
+  // Support for .ejs in pages
+  if (pages === true) info.pagesExtensions.push('.ejs')
 
-    const buildPath = createBuildPath(path.parse(file), info.configs.buildPath)
+  // Support for .ejs in contents
+  if (contents === true) info.contentsExtensions.push('.ejs')
 
-    const pages = {
-      type: 'raw',
-      content: raw,
-      source: path.relative(info.cwd, file),
-      link: getLink(info.configs, buildPath),
-      values: { stats },
-      buildPath: buildPath,
-    }
+  // Renderer to use to render ejs to html
+  info.renderer['.ejs'] = (raw, context, filename) => {
+    const options = { ...ejsOptions, filename, context }
 
-    ejsOptions.filename = file
-    ejsOptions.context = pages
-
-    info.renderer['.ejs'] = (raw, context) => {
-      return ejs.render(raw, context, ejsOptions)
-    }
-
-    info.ignoreExtensions.push('.ejs')
-
-    pagesArray.push(pages)
-  }
-}
-
-function createBuildPath(parsedPath, buildPath) {
-  let newBuildPath
-  if (parsedPath.name === 'index') {
-    newBuildPath = path.join(buildPath, 'index.html')
-  } else {
-    newBuildPath = path.join(buildPath, parsedPath.name, 'index.html')
-  }
-  return newBuildPath
-}
-
-function getLink(configs, buildPath) {
-  const link = path.relative(configs.buildPath, buildPath).slice(0, -10)
-
-  this.link = url.format(link)
-
-  if (!this.link.startsWith('/')) {
-    this.link = '/' + this.link
-  }
-
-  return this.link
-}
-
-function allFiles(dirPath, arrayOfFiles = []) {
-  try {
-    const files = fs.readdirSync(dirPath)
-
-    files.forEach(file => {
-      const pathTo = path.join(dirPath, file)
-      if (fs.statSync(pathTo).isDirectory()) {
-        arrayOfFiles = allFiles(pathTo, arrayOfFiles)
-      } else {
-        if (path.parse(file).ext === '.ejs') arrayOfFiles.push(pathTo)
-      }
-    })
-
-    return arrayOfFiles
-  } catch (e) {
-    console.log(e.message)
-    throw new Error(`Error while finding all .ejs files in ${dirPath}`)
+    return ejs.render(raw, context, options)
   }
 }
